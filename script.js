5cd61eaf8d45736af7e23dbec9b51d6588e09322
Hong Shik Branden Kim <kimploo@gmail.com>
2022-07-19 19:11:25 +0900

    lint

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 let data;
-const dataFromLocalStorage = localStorage.getItem('agoraStatesDiscussions')
+const dataFromLocalStorage = localStorage.getItem("agoraStatesDiscussions");
 if (dataFromLocalStorage) {
-  data = JSON.parse(dataFromLocalStorage)
+  data = JSON.parse(dataFromLocalStorage);
 } else {
-  data = agoraStatesDiscussions.slice()
+  data = agoraStatesDiscussions.slice();
 }
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
@@ -49,9 +49,10 @@ const convertToDiscussion = (obj) => {
 
 // data 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element, from, to) => {
-  console.log(from, to)
+  console.log(from, to);
   if (!from && !to) {
-    from = 0; to = data.length - 1
+    from = 0;
+    to = data.length - 1;
   }
   // 다 지우고 배열에 있는 내용 다 보여주기
   while (element.firstChild) {
@@ -64,56 +65,57 @@ const render = (element, from, to) => {
 };
 
 // 페이지네이션을 위한 변수
-let limit = 10, page = 1
+let limit = 10,
+  page = 1;
 
 // ul 요소에 data 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul, 0, limit);
 
 const getPageStartEnd = (limit, page) => {
-  const len = data.length - 1
+  const len = data.length - 1;
   let pageStart = Number(page - 1) * Number(limit);
   let pageEnd = Number(pageStart) + Number(limit);
   if (page <= 0) {
     pageStart = 0;
   }
   if (pageEnd >= len) {
-    // pageStart = Number(page - 2) * Number(limit)
-    pageEnd = len
+    pageEnd = len;
   }
   return { pageStart, pageEnd };
 };
 
-const buttons = document.querySelector('.buttons');
-buttons.children[0].addEventListener('click', (event) => {
+const buttons = document.querySelector(".buttons");
+buttons.children[0].addEventListener("click", () => {
   if (page > 1) {
     page = page - 1;
   }
-  const { pageStart, pageEnd } = getPageStartEnd(limit, page)
+  const { pageStart, pageEnd } = getPageStartEnd(limit, page);
   render(ul, pageStart, pageEnd);
-})
+});
 
-buttons.children[1].addEventListener('click', () => {
-  if ((limit * page) < data.length - 1) {
+buttons.children[1].addEventListener("click", () => {
+  if (limit * page < data.length - 1) {
     page = page + 1;
   }
-  const { pageStart, pageEnd } = getPageStartEnd(limit, page)
+  const { pageStart, pageEnd } = getPageStartEnd(limit, page);
   render(ul, pageStart, pageEnd);
-})
+});
 
-buttons.children[2].addEventListener('click', () => {
-  localStorage.removeItem('agoraStatesDiscussions');
+buttons.children[2].addEventListener("click", () => {
+  localStorage.removeItem("agoraStatesDiscussions");
   data = agoraStatesDiscussions.slice();
-  limit = 10; page = 1;
-  render(ul, 0, limit)
-})
+  limit = 10;
+  page = 1;
+  render(ul, 0, limit);
+});
 
 // 문서의 내용을 확인해야 합니다.
 const form = document.querySelector("form.form");
 const author = form.querySelector("div.form__input--name > input");
 const title = form.querySelector("div.form__input--title > input");
 const textbox = form.querySelector("div.form__textbox > textarea");
-  
+
 // 문서를 제출해야 합니다.
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -131,8 +133,8 @@ form.addEventListener("submit", (event) => {
   data.unshift(obj);
 
   // 로컬스토리지에 저장
-  localStorage.setItem('agoraStatesDiscussions', JSON.stringify(data))
+  localStorage.setItem("agoraStatesDiscussions", JSON.stringify(data));
 
   // 렌더링
   render(ul, 0, limit);
-})
+});
